a926b026219bcd5ef73c8319a123d3fc8b318382
rlokc <kirik1007@gmail.com>
2017-02-07 13:30:47 +0300

    Maven refactoring, adding dependency inclusion

---
+++ ru.rlokc.bachparse.parent/ru.rlokc.bachparse.plugins/ru.rlokc.bachparse.consumer/src/main/java/ru/rlokc/bachparse/consumer/ConsumerActivator.java
@@ -0,0 +1,24 @@
+package ru.rlokc.bachparse.consumer;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
+
+import ru.rlokc.bachparse.service.api.IModelProvider;
+
+public class ConsumerActivator implements BundleActivator{
+	
+	private BundleContext context;
+	private IModelProvider provider;
+	
+	public void start(BundleContext context) {
+		this.context = context;
+		ServiceReference<?> reference = context.getServiceReference(IModelProvider.class.getName());
+		provider = (IModelProvider) context.getService(reference);
+		System.out.println("Consumer launched, got a provider");
+	}
+	
+	public void stop(BundleContext context) {
+		System.out.println("Consumer stopped");
+	}
+}
